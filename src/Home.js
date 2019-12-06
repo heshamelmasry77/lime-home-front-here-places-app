1ec1a1885ece43ecfef81affad20ef2a46f0298b
hesham <heshamelmasry1992@gmail.com>
2019-12-06 04:05:05 +0200

    getting initial data and make sure the API call is working.

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {Component} from 'react';
+import axios from 'axios';
 
 class Home extends Component {
   componentDidMount() {
     window.scrollTo(0, 0);
     console.log('Home page');
+    console.log(process.env.REACT_APP_APP_ID);
+    axios.get(`https://places.cit.api.here.com/places/v1/autosuggest?at=40.74917,-73.98529&q=hotels&app_id=${process.env.REACT_APP_APP_ID}&app_code=${process.env.REACT_APP_APP_CODE}
+`)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
